bb2f19501f404988cce031b83edb43b9ef911764
cesarioalmeida <cesario.de.almeida@gmail.com>
2017-03-11 18:07:47 +0800

    Added complete solution

---
+++ StressTesting/ExtendedListBoxItembackup.cs
@@ -0,0 +1,28 @@
+﻿//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Media;
+
+//namespace StressTesting
+//{
+//    class ExtendedListBoxItembackup : ListBoxItem
+//    {
+//        public ImageSource ImageSource
+//        {
+//            get { return (ImageSource)GetValue(ImageSourceProperty); }
+//            set { SetValue(ImageSourceProperty, value); }
+//        }
+
+//        public static readonly DependencyProperty ImageSourceProperty =
+//            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ExtendedListBoxItembackup), new PropertyMetadata(default(ImageSource)));
+
+//        public string Text
+//        {
+//            get { return (string)GetValue(TextProperty); }
+//            set { SetValue(TextProperty, value); }
+//        }
+
+//        public static readonly DependencyProperty TextProperty =
+//            DependencyProperty.Register("Text", typeof(string), typeof(ExtendedListBoxItembackup), new PropertyMetadata(string.Empty));
+
+//    }
+//}
